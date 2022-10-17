1ed93e243ba71dd7f708ff304fcf8f27b8622474
Maraj Hussain <maraj@markowate.com>
2022-10-17 17:35:44 +0530

    compose demo code

---
+++ app/src/main/java/com/maraj/composedemo/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.maraj.composedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.maraj.composedemo.ui.theme.ComposeDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting("Maraj Hussain")
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!")
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        Greeting("Maraj Hussain")
+    }
+}
